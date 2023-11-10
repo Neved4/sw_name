41ebe578286dfc4d2fbdf3af236c0bc498372ba1
Neveda <63655535+Neved4@users.noreply.github.com>
2023-11-10 18:01:12 +0100

    Add support for OS X and multiword names

--- src/sw_name.sh
+++ src/sw_name.sh
@@ -6,18 +6,15 @@ sw_name() {
 	readonly license="$app/Contents/Resources/en.lproj/OSXSoftwareLicense.rtf"
 
 	awk '/SOFTWARE LICENSE AGREEMENT FOR/ {
-		print substr($NF, 1, length($NF)-1)
+		gsub(/.*(macOS|OS X) |\\|.*SOFTWARE LICENSE AGREEMENT FOR /, "")
+		print
 	}' "$license"
-}
-
-print_sw_name() {
-	printf 'ReleaseName:\t\t%s\n' "$(sw_name)"
 }
 
 sw_vers_name() {
  	if [ $# -eq 0 ]
 	then
-		print_sw_name
+		printf 'ReleaseName:\t\t%s\n' "$(sw_name)"
 		/usr/bin/sw_vers
 		return 0
 	fi
