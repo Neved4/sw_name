f8288942429be223f056c03dd8490f0d1042c928
Neveda <63655535+Neved4@users.noreply.github.com>
2023-11-13 00:04:18 +0100

    Add better visual output

--- tools/test.sh
+++ tools/test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -Cefu
 
+reset='\033[0m'
+ blue='\033[34m'
+
 findlic() {
 	find -Es tools/samples -type f -regex '.*\.(rtf|html)' | sort -V
 }
@@ -16,7 +19,7 @@ main() {
 	do
 		ext=${file##*.} path=${file##*/} name=${path% *}
 
-		printf %-14s%-5s%s "- $name" "$ext" \
+		printf "%-14s%-5s%-2s $blue%-2s$reset" "- $name" "$ext" "=>" \
 			"$(DARWIN_LICENSE=$file src/sw_name.sh |
 				awk '/ReleaseName/ {print $2, $3}')"
 		printf '\n'
