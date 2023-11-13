56a6c07e0928dadbda5c0ebe57363adffad64ee0
Neveda <63655535+Neved4@users.noreply.github.com>
2023-11-13 23:14:21 +0100

    Refactor var naming in test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -21,32 +21,31 @@ iter() {
 		 ver=${path##* }
 
 		color=$blue
-		friendly=$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)
+		release=$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)
 
 		for i in '10.9 Mavericks' '10.10 Yosemite' '10.11 El Capitan' \
 		    '10.12 Sierra' '10.13 High Sierra' '10.14 Mojave' '10.15 Catalina' \
 			'11 Big Sur' '12 Monterey' '13 Ventura' '14 Sonoma'
 		do
-			i_ver=${i%% *} i_friendly=${i#* }
+			i_ver=${i%% *} i_release=${i#* }
 
 			if [ "$ver" = "$i_ver" ]
 			then
-				[ "$friendly" = "$i_friendly" ] &&
+				[ "$release" = "$i_release" ] &&
 				color=$green || color=$under$red
 			fi
 		done
 
 		printf '  %5s %-6s%-2s %b%-2s%b\n' "$name" "$ver" "=>" "$color" \
-    		"$friendly" "$reset"
+    		"$release" "$reset"
 	done
 }
 
 main() {
 	dir='tools/samples/OSXSoftwareLicense'
 
-	findf html && iter
-	printf '\n'
-	findf rtf && iter
+	findf html && iter; printf '\n'
+	findf rtf  && iter
 }
 
 main
