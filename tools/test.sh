f312c5310aedf3e112235313e69bfe18c4163bb1
Neveda <63655535+Neved4@users.noreply.github.com>
2023-11-13 13:24:24 +0100

    Refactor visual output

--- tools/test.sh
+++ tools/test.sh
@@ -4,25 +4,32 @@ set -Cefu
 reset='\033[0m'
  blue='\033[34m'
 
-findlic() {
-	find -Es tools/samples -type f -regex '.*\.(rtf|html)' | sort -V
+iter() {
+	for file in $files
+	do
+		 ext=${file##*.}
+		path=${file##*/}
+		path=${path%.*}
+		name=${path% *}
+		 ver=${path##* }
+
+		printf '%s %5s %-6s%-2s %b%-2s%b\n' "-" "$name" "$ver" "=>" "$blue" \
+    		"$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)" "$reset"
+	done
 }
 
 main() {
-	licenses=$(findlic)
 	IFS='
 	'
 
-	printf '%s\n' 'Parsed files:'
-
-	for file in $licenses
-	do
-		ext=${file##*.} path=${file##*/} name=${path% *}
-
-		printf "%-7s%-12s%-2s $blue%-2s$reset" "- $ext" "$name" "=>" \
-			"$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)"
-		printf '\n'
-	done
+	dir='tools/samples/OSXSoftwareLicense'
+	printf 'html:\n'
+	files=$(find -Es $dir -type f -regex '.*\.(html)' | sort -V)
+	iter
+	printf '\n'
+	printf 'rtf:\n'
+	files=$(find -Es $dir -type f -regex '.*\.(rtf)' | sort -V)
+	iter
 }
 
 main
