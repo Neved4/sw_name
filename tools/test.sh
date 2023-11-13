dfc759357f3a9dea07f0e7f03b77c6a15c8998fa
Neveda <63655535+Neved4@users.noreply.github.com>
2023-11-13 15:18:29 +0100

    Add string comparison test with colored diff

--- tools/test.sh
+++ tools/test.sh
@@ -2,6 +2,8 @@
 set -Cefu
 
 reset='\033[0m'
+  red='\033[31m'
+green='\033[32m'
  blue='\033[34m'
 
 iter() {
@@ -13,8 +15,23 @@ iter() {
 		name=${path% *}
 		 ver=${path##* }
 
-		printf '%s %5s %-6s%-2s %b%-2s%b\n' "-" "$name" "$ver" "=>" "$blue" \
-    		"$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)" "$reset"
+		color=$blue
+		friendly=$(DARWIN_LICENSE=$file src/sw_name.sh -releaseName)
+
+		for i in '10.9 Mavericks' '10.10 Yosemite' '10.11 El Capitan' \
+		    '10.12 Sierra' '10.13 High Sierra' '10.14 Mojave' '10.15 Catalina' \
+			'11 Big Sur' '12 Monterey' '13 Ventura' '14 Sonoma'
+		do
+			i_ver=${i%% *} i_friendly=${i#* }
+
+			if [ "$ver" = "$i_ver" ]
+			then
+				[ "$friendly" = "$i_friendly" ] && color=$green || color=$red
+			fi
+		done
+
+		printf '%s %5s %-6s%-2s %b%-2s%b\n' "-" "$name" "$ver" "=>" "$color" \
+    		"$friendly" "$reset"
 	done
 }
 
